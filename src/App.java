e63827401c0b805af74ff43ab03d697a8c8479bf
David <256754@vut.cz>
2025-03-28 13:28:51 +0100

    Hash na jmeno

--- src/App.java
+++ src/App.java
@@ -1,10 +1,10 @@
-import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("David je ghej");
         System.out.println("David je ghej");
         Student stud = new TLI("david", "chuj", "01.01.2001");
-        
+        Student studIBE = new IBE("Jorge", "BAD", "01.01.2001");
+        System.out.println(studIBE.specialAbility());
     }
 }
--- src/IBE.java
+++ src/IBE.java
@@ -1,3 +1,6 @@
+
+import java.util.Random;
+
 public class IBE extends Student {
     public IBE(String name, String surname, String birthDate) {
         super(name, surname, birthDate);
@@ -6,7 +9,21 @@ public class IBE extends Student {
     @Override
     public String specialAbility()
     {
-        String hash = "";
-        return hash;
+        Random rand = new Random();
+        int hash = rand.nextInt(11);
+        int HashNumber;
+        String hashName = "";
+        int pismenoNmbr;
+
+        char []pismeno= getName().toCharArray();
+
+
+        for (int i = 0; i < pismeno.length; i++) {
+            pismenoNmbr = Character.getNumericValue(pismeno[i]);
+            HashNumber = pismenoNmbr*hash;
+            hashName = String.valueOf(HashNumber);
+        }
+
+        return hashName;
     }
 }
