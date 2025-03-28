dc6c862cd80be28c8b35fdbb435c9f885185c8ae
Jirka <Jirka@POZNAMKOVYBLOK>
2025-03-28 12:03:26 +0100

    test

--- src/Student.java
+++ src/Student.java
@@ -1,15 +1,24 @@
 import java.util.List;
 
 public abstract class Student {
-	private String Name;
-	private String Surname;
-	private static int ID;
-	private String BirthDate;
-	private List<Student> StudentList;
+	protected String Name;
+	protected String Surname;
+	protected static int ID;
+	protected String BirthDate;
+	protected List<Student> StudentList;
+	protected List<Integer> StudentMarks;
 	
 	public Student(String name, String surname, String birthDate) {
 		Name=name;
 		Surname=surname;
 		BirthDate=birthDate;
+	}
+
+	public boolean AddMark(Integer mark) {
+		if (mark >= 1 && mark <=5) {
+			StudentMarks.add(mark);
+			return true;
+		}
+		return false;
 	}
 }
