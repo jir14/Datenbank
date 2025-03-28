a9d215a0f206c0b281025cf8e49b720681dbebdb
David <256754@vut.cz>
2025-03-28 13:01:03 +0100

    MorseCode

--- src/TLI.java
+++ src/TLI.java
@@ -20,27 +20,27 @@ public class TLI extends Student {
         MorseCode.put('c', "-.-.");
         MorseCode.put('d', "-..");
         MorseCode.put('e', ".");
-        MorseCode.put('f', "...");
-        MorseCode.put('g', "...");
-        MorseCode.put('h', "...");
-        MorseCode.put('i', "...");
-        MorseCode.put('j', "...");
-        MorseCode.put('k', "...");
-        MorseCode.put('l', "...");
-        MorseCode.put('m', "...");
-        MorseCode.put('n', "...");
-        MorseCode.put('o', "...");
-        MorseCode.put('p', "...");
-        MorseCode.put('q', "...");
-        MorseCode.put('r', "...");
+        MorseCode.put('f', "..-.");
+        MorseCode.put('g', "--.");
+        MorseCode.put('h', "....");
+        MorseCode.put('i', "..");
+        MorseCode.put('j', ".---");
+        MorseCode.put('k', "-.-");
+        MorseCode.put('l', ".-..");
+        MorseCode.put('m', "--");
+        MorseCode.put('n', "-.");
+        MorseCode.put('o', "---");
+        MorseCode.put('p', ".--.");
+        MorseCode.put('q', "--.-");
+        MorseCode.put('r', ".-.");
         MorseCode.put('s', "...");
-        MorseCode.put('t', "...");
-        MorseCode.put('u', "...");
-        MorseCode.put('v', "...");
-        MorseCode.put('w', "...");
-        MorseCode.put('x', "...");
-        MorseCode.put('y', "...");
-        MorseCode.put('z', "...");
+        MorseCode.put('t', "-");
+        MorseCode.put('u', "..-");
+        MorseCode.put('v', "...-");
+        MorseCode.put('w', ".--");
+        MorseCode.put('x', "-..-");
+        MorseCode.put('y', "-.--");
+        MorseCode.put('z', "--..");
 
         for (int i = 0; i < nameArray.length; i++)
         {
