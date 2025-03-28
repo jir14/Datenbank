bfd88425367514184948174be1598027706e7505
David <256754@vut.cz>
2025-03-28 16:01:40 +0100

    Start GUI Katastrofální porážka

--- src/App.java
+++ src/App.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -13,5 +13,55 @@ public class App {
         for (Student stud : db.getOrderedStudents(1)) {
             System.out.println(stud.getSurname());
         }
+
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.println("1. Vytvoreni databaze");
+        System.out.println("2. Vytvoreni studenta");
+        System.out.println("3. Vybrat studenta");
+        System.out.println("4. Abecedni vypis studentu");
+        System.out.println("5. Informace o oboru");
+
+        int volba = sc.nextInt();
+
+        switch(volba)
+        {
+            //1. Vytvoreni databaze
+            case 1:
+                Databaze dtb = new Databaze();
+                break;
+
+            //2. Vytvoreni studenta
+            case 2:
+                //Volba oboru
+                System.out.println("Zadejte nazev oboru: [1. TLI; 2. IBE]: ");
+                int obor = sc.nextInt();
+                
+                //Zadani jmena
+                System.out.println("Zadejte jmeno studenta: ");
+                String name = sc.next();
+
+                //Zadani prijmeni
+                System.out.println("Zadejte jmeno studenta: ");
+                String surname = sc.next();
+
+                //Zadani data narozeni
+                System.out.println("Zadejte jmeno studenta: ");
+                String birthDate = sc.next();
+
+                //Pripsani
+                /*
+                if (dtb != null)
+                {
+                    System.out.println("Databaze nebyla vytvorena, vytvareni databaze");
+                    Databaze dtb = new Databaze();
+                }
+                else
+                {
+                    dtb.setStudent(obor, name, surname, birthDate);
+                }
+                */
+                
+        }   
     }
 }
